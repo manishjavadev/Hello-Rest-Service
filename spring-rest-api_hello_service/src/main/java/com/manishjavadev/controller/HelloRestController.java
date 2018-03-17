ede2224f6b4902e8ee2beee06d81c7f58f8d7ab2
Manish <manishjavadev.com>
2018-03-17 21:53:01 +0530

    code chnages

--- spring-rest-api_hello_service/src/main/java/com/manishjavadev/controller/HelloRestController.java
+++ spring-rest-api_hello_service/src/main/java/com/manishjavadev/controller/HelloRestController.java
@@ -1,5 +1,6 @@
 package com.manishjavadev.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
+@RequestMapping("/helloService")
 public class HelloRestController {
 
-	@RequestMapping(value = { "/ping" }, method = RequestMethod.GET)
-	public boolean ping() {
+	@RequestMapping(value = { "/ping" }, method = RequestMethod.GET, produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_VALUE })
+	public Boolean ping() {
 		return true;
 	}
 
-	@RequestMapping(value = { "/testhello" }, method = RequestMethod.GET)
-	public String testHello() {
+	@RequestMapping(value = { "/helloPing" }, method = RequestMethod.GET)
+	public String helloPing() {
 		return "Hello Java Developement Blog";
 	}
 }
